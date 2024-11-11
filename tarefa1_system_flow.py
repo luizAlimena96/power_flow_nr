17839b37a673552b6ecaff5f939a87ecba428d91
luizAlimena96 <luizalimena@outlook.com>
2024-11-10 21:53:23 -0300

    update_1011

--- tarefa1_system_flow.py
+++ tarefa1_system_flow.py
@@ -306,4 +306,3 @@ print("\nNEWTON-RAPHSON METHOD")
 print("Voltages (magnitude and angle) after convergence:")
 for i, bus in enumerate(buses):
     print(f"Bus {bus['num']}: |V| = {V_nr[i]:.4f} p.u., θ = {delta_nr[i]:.4f}°")
-    
